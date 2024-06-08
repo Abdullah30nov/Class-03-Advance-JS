c90e13b5dd0c887e28836a467b9b3ea5af76959f
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-06-08 21:22:52 +0500

    Advance js class 3 done

---
+++ app.js
@@ -0,0 +1,43 @@
+// // Dissaperaing object child
+// let {edu,age,city,user1} = {
+//     edu:"matric",
+//     age: 20,
+//     city: "Karachi",
+//      user1 : {
+//         u1name: "Abdullah",
+//         u1age: 20,
+//         u1city: "Karachi"
+//     }
+// }
+// let obj = {edu,age,city,user1};
+// // let {user1} = obj.user1
+// console.log(obj);
+
+
+//Sperad Operators
+// let arr1 = [1,2,3,4,5];
+// // console.log(arr1);
+// let arr2 = [6,7,8,9];
+// // console.log(arr2);
+// let merge = [...arr1,...arr2];
+// console.log(merge);
+
+
+// let user1 = {
+//     u1name: "Abdullah",
+//     u1age: 20,
+//     u1city: "Karachi"
+// }
+// let user2 = {
+//     u2name: "Bilal",
+//     u2age: 22,
+//     u2city: "Lahore"
+// }
+// let merge = {...user1,...user2};
+// console.log(merge);
+
+// let num1 = [1,2,3,4,5,"abdullah"];
+// // let num2 = [6,7,8,9,"bilal"];
+// num2 = [...num1];
+// num2[6] = "arham"
+// console.log(num1,num2);
